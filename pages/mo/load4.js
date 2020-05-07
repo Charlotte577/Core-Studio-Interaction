497c76ecb2adf0fc77355d17161939fc3c8c20ae
Charlotte577 <Charlotte577@users.noreply.github.com>
2020-05-06 21:21:25 -0400

    0

---
+++ pages/mo/load4.js
@@ -0,0 +1,18 @@
+var didWait = false;
+// DocRaptor polls docraptorJavaScriptFinished until it is true
+docraptorJavaScriptFinished = function() {
+  if (! didWait) {
+    setTimeout(function(){
+      didWait = true;
+    }, 10000);
+    return false;
+  }
+  // It's been 10 seconds, let's return true and make the PDF!
+  return true;
+}
+
+
+
+setTimeout(function () {
+    location.href = "page3.html";
+}, 2000);
